7d24ac1481b6366ae47c588d411772a12fa80859
Joe Gambino <joe@expensify.com>
2021-12-08 14:24:24 -0800

    remove unnecessary core

--- .github/actions/javascript/checkBundleVersionStringMatch/checkBundleVersionStringMatch.js
+++ .github/actions/javascript/checkBundleVersionStringMatch/checkBundleVersionStringMatch.js
@@ -1,4 +1,3 @@
-const core = require('@actions/core');
 const {execSync} = require('child_process');
 
 const bundleVersion = execSync(`grep -A1 'CFBundleVersion' ../../../../test.plist | grep -v 'CFBundleVersion' | sed 's|[</string>,]||g')`).toString().trim();
@@ -10,11 +9,8 @@ console.log(`Short Bundle Version: ${shortBundleVersion}`);
 const hasValue = shortBundleVersion && bundleVersion;
 if (!hasValue) {
     console.log('Failed to get Bundle Versions from plist');
-    core.setOutput('BUNDLE_VERSIONS_MATCH', false);
 } else if (shortBundleVersion !== (bundleVersion.split('-') || [''])[0]) {
     console.log('Bundle Versions do not match');
-    core.setOutput('BUNDLE_VERSIONS_MATCH', false);
 } else {
     console.log('Bundle Versions match');
-    core.setOutput('BUNDLE_VERSIONS_MATCH', true);
 }
