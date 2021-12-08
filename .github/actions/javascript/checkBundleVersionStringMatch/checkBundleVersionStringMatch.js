d87dad95d566860db1c5c0f9ebe92ea96d242f77
Joe Gambino <joe@expensify.com>
2021-12-08 12:23:33 -0800

    Add checkBundleVersionString

---
+++ .github/actions/javascript/checkBundleVersionStringMatch/checkBundleVersionStringMatch.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core');
+const {execSync} = require('child_process');
+
+const bundleVersion = execSync(`grep -A1 'CFBundleVersion' ../../../../test.plist | grep -v 'CFBundleVersion' | sed 's|[</string>,]||g')`).toString().trim();
+const shortBundleVersion = execSync(`grep -A1 'CFBundleShortVersionString' ../../../../test.plist | grep -v 'CFBundleShortVersionString' | sed 's|[</string>,]||g')`).toString().trim();
+
+console.log(`Bundle Version: ${bundleVersion}`);
+console.log(`Short Bundle Version: ${shortBundleVersion}`);
+
+const hasValue = shortBundleVersion && bundleVersion;
+if (!hasValue) {
+    console.log('Failed to get Bundle Versions from plist');
+    core.setOutput('BUNDLE_VERSIONS_MATCH', false);
+} else if (shortBundleVersion !== (bundleVersion.split('-') || [''])[0]) {
+    console.log('Bundle Versions do not match');
+    core.setOutput('BUNDLE_VERSIONS_MATCH', false);
+} else {
+    console.log('Bundle Versions match');
+    core.setOutput('BUNDLE_VERSIONS_MATCH', true);
+}
