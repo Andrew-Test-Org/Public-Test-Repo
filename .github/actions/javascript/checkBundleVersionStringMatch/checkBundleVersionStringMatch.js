9280954ef4dc0ab6af529466d176af30e5baf877
Joe Gambino <joe@expensify.com>
2021-12-08 14:37:49 -0800

    more tests

--- .github/actions/javascript/checkBundleVersionStringMatch/checkBundleVersionStringMatch.js
+++ .github/actions/javascript/checkBundleVersionStringMatch/checkBundleVersionStringMatch.js
@@ -1,7 +1,12 @@
 const {execSync} = require('child_process');
 
-const bundleVersion = execSync(`grep -A1 'CFBundleVersion' ../../test.plist | grep -v 'CFBundleVersion' | sed 's|[</string>,]||g'`).toString().trim();
-const shortBundleVersion = execSync(`grep -A1 'CFBundleShortVersionString' ../../test.plist | grep -v 'CFBundleShortVersionString' | sed 's|[</string>,]||g'`).toString().trim();
+const output = execSync(`pwd`).toString();
+console.log(output);
+console.log('Find output');
+const findOutput = execSync('find ./../.. -name test.plist').toString();
+console.log(findOutput);
+const bundleVersion = execSync(`grep -A1 'CFBundleVersion' ./test.plist | grep -v 'CFBundleVersion' | sed 's|[</string>,]||g'`).toString().trim();
+const shortBundleVersion = execSync(`grep -A1 'CFBundleShortVersionString' ./test.plist | grep -v 'CFBundleShortVersionString' | sed 's|[</string>,]||g'`).toString().trim();
 
 console.log(`Bundle Version: ${bundleVersion}`);
 console.log(`Short Bundle Version: ${shortBundleVersion}`);
--- .github/actions/javascript/checkBundleVersionStringMatch/index.js
+++ .github/actions/javascript/checkBundleVersionStringMatch/index.js
@@ -52,8 +52,13 @@ var __webpack_exports__ = {};
 (() => {
 const {execSync} = __nccwpck_require__(81);
 
-const bundleVersion = execSync(`grep -A1 'CFBundleVersion' ../../test.plist | grep -v 'CFBundleVersion' | sed 's|[</string>,]||g'`).toString().trim();
-const shortBundleVersion = execSync(`grep -A1 'CFBundleShortVersionString' ../../test.plist | grep -v 'CFBundleShortVersionString' | sed 's|[</string>,]||g'`).toString().trim();
+const output = execSync(`pwd`).toString();
+console.log(output);
+console.log('Find output');
+const findOutput = execSync('find ./../.. -name test.plist').toString();
+console.log(findOutput);
+const bundleVersion = execSync(`grep -A1 'CFBundleVersion' ./test.plist | grep -v 'CFBundleVersion' | sed 's|[</string>,]||g'`).toString().trim();
+const shortBundleVersion = execSync(`grep -A1 'CFBundleShortVersionString' ./test.plist | grep -v 'CFBundleShortVersionString' | sed 's|[</string>,]||g'`).toString().trim();
 
 console.log(`Bundle Version: ${bundleVersion}`);
 console.log(`Short Bundle Version: ${shortBundleVersion}`);
