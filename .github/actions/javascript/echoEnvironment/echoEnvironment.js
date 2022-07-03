d2c2968f2cc19c02a1492cc9f017a88765635a79
roryabraham <roryabraham@users.noreply.github.com>
2022-07-03 16:10:22 +0000

    deploy: a31a571db518fbbd4ba1a6bf118e432e8b71e816

--- .github/actions/javascript/echoEnvironment/echoEnvironment.js
+++
@@ -1,8 +0,0 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-
-if (process.env.MY_BOOL) {
-    console.log('my bool is true');
-} else {
-    console.log('my bool is false');
-}
