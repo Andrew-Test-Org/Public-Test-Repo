8d87c4fc6aef7581f6b29956297e83467862842f
Andrew Gable <andrew@expensify.com>
2022-07-07 13:02:06 -0600

    Update test

--- .github/actions/javascript/contributorChecklist/contributorChecklist.js
+++ .github/actions/javascript/contributorChecklist/contributorChecklist.js
@@ -51,11 +51,21 @@ const checklist = `#### Contributor (PR Author) Checklist
 
 const diff = Diff.diffLines(checklist, comment);
 
+if (!diff) {
+    console.log('No diff, checklist is complete!')
+    return
+}
+
+console.log('Checklist not complete, difference from expected:')
+
 diff.forEach((part) => {
     // green for additions, red for deletions
     // grey for common parts
-    const color = part.added ? 'green' : part.removed ? 'red' : 'grey';
-    process.stderr.write(part.value[color]);
+    const color = part.added ? 'green' : part.removed ? 'red' : null;
+
+    if (part.value[color]) {
+        console.log(part.value[color])
+    }
 });
 
-console.log();
+core.setFailed("Checklist not complete");
