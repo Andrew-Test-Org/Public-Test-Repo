813b3cd58767fd9dd6c9ef8ced1d4c3f71ab97f1
Andrew Gable <andrew@expensify.com>
2022-07-07 14:58:22 -0600

    More debugging

--- .github/actions/javascript/contributorChecklist/index.js
+++ .github/actions/javascript/contributorChecklist/index.js
@@ -15194,13 +15194,16 @@ const contributorPlusChecklist = `- [x] I verified the correct issue is linked i
 - [x] If the PR modifies a generic component, I tested and verified that those changes do not break usages of that component in the rest of the App (i.e. if a shared library or component like \`Avatar\` is modified, I verified that \`Avatar\` is working as expected in all cases)
 - [x] If the PR modifies a component related to any of the existing Storybook stories, I tested and verified all stories for that component are still working as expected.`;
 
+console.log(issue);
+
 GitHubUtils.octokit.rest.issues.listComments({
     owner: GitHubUtils.GITHUB_OWNER,
     repo: GitHubUtils.APP_REPO,
     issue_number: issue,
     per_page: 100,
 }).then(({data}) => {
-    console.log(JSON.stringify(github.context.payload));
+    console.log(JSON.stringify(github.context.payload.issue))
+
     console.log(JSON.stringify(data));
     let comments = data.map(comment => comment.body).toString();
     let trimmedComment = comments.replace(/(\s|\r\n|\n|\r)/gm, "");
